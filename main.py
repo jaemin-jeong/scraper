ac2dd0cb187c8dc7ca072c2fd720f6403bf97a47
jaemin-jeong <jaemin-jeong@naver.com>
2020-07-01 15:09:51 +0900

    🚧 indeed scrap basic work
    - 타이틀, 회사명, 위치, 지원링크 긁어오기
    - 함수 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from indeed import extract_indeed_page, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
 
-max_indeed_pages = extract_indeed_page()
-extract_indeed_jobs(max_indeed_pages)
+indeed_jobs = get_indeed_jobs()
+
+print(indeed_jobs)
