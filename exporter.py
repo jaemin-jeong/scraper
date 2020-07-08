3e52e05b1eb4bfeba38afe8cbb37dcb88ddcc6c2
jaemin-jeong <jaemin-jeong@naver.com>
2020-07-08 21:44:09 +0900

    🚸 export csv file
    - 검색 결과를 csv파일로 다운로드 받을 수 있게 함

---
+++ exporter.py
@@ -0,0 +1,7 @@
+import csv
+
+def save_to_file(jobs):
+  file = open('jobs.csv', mode='w')
+  writer = csv.writer(file)
+  for job in jobs:
+    writer.writerow(list(job.values()))
--- server/index.py
+++ server/index.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
+import logging
+# from exporter import save_to_file
 # from ..indeed import get_jobs 
 
-# 참고: https://myjorney.tistory.com/52
+
 if __name__ == '__main__':
     if __package__ is None:
         import sys
@@ -9,8 +11,10 @@ if __name__ == '__main__':
         print(path.dirname( path.dirname( path.abspath(__file__) ) ))
         sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
         from indeed import get_jobs
+        from exporter import save_to_file
     else:
         from ..indeed import get_jobs
+        from ..exporter import save_to_file
 
 app = Flask('index')
 
@@ -20,13 +24,13 @@ db = {}
 def home():
   return render_template('home.html')
 
-@app.route('/contact')
-def contact():
-  return 'contact me!'
+# @app.route('/contact')
+# def contact():
+#   return 'contact me!'
 
-@app.route('/<username>')
-def username(username):
-  return f'Hello your name is {username}!'
+# @app.route('/<username>')
+# def username(username):
+#   return f'Hello your name is {username}!'
 
 @app.route('/report')
 def report():
@@ -46,6 +50,23 @@ def report():
     resultsNumber=len(jobs),
     jobs=jobs,
   )
-  
+
+@app.route('/export')
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file('job.csv')
+  except Exception as e:
+    logger = logging.getLogger('GET LOGGER')
+    logger.error('Failed to do something: ' + str(e))
+    return redirect('/')
+
 
 app.run(host='localhost')
